0b7d65028d37633f22a2a9a0bcc1cc2db212366b
Lord-Y <Lord-Y@users.noreply.github.com>
2021-05-05 17:59:25 +0200

    [cmd|cypress|cypress] - Add requirements to execute "cypress run" command and units testing

--- cmd/cypress/cypress.go
+++ cmd/cypress/cypress.go
@@ -3,6 +3,14 @@ package cypress
 
 import (
 	"fmt"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
+	"sync"
+
+	"github.com/Lord-Y/cypress-parallel-cli/git"
+	"github.com/rs/zerolog/log"
 )
 
 // Cypress requirements to run cypress command
@@ -21,5 +29,129 @@ type Cypress struct {
 
 // Run will run cypress command
 func (c *Cypress) Run() {
-	fmt.Printf("run\n")
+	var (
+		gc git.Repository
+	)
+
+	gc.Repository = c.Repository
+	gc.Username = c.Username
+	gc.Password = c.Password
+	gc.Branch = c.Branch
+
+	gitdir, err := gc.Clone()
+	if err != nil {
+		log.Fatal().Err(err).Msg("Error occured while cloning git repository")
+		return
+	}
+	defer os.RemoveAll(gitdir)
+	log.Debug().Msgf("Git temp dir %s", gitdir)
+
+	err = os.Chdir(gitdir)
+	if err != nil {
+		log.Fatal().Err(err).Msg("Error occured while cloning git repository")
+		return
+	}
+
+	if c.ConfigFile != "" {
+		var info os.FileInfo
+		if info, err = os.Stat(c.ConfigFile); os.IsNotExist(err) {
+			log.Fatal().Err(err).Msgf("Error occured while checking config file %s", c.ConfigFile)
+			return
+		}
+		if !info.Mode().IsRegular() {
+			log.Fatal().Err(err).Msgf("Error occured while checking config file %s", c.ConfigFile)
+			return
+		}
+	}
+
+	execUninstallCmd := exec.Command(
+		"npm",
+		"uninstall",
+		"cypress",
+		"&&",
+		"npm",
+		"uninstall",
+		"-D",
+		"cypress",
+	)
+
+	if err := execUninstallCmd.Run(); err != nil {
+		log.Fatal().Err(err).Msg("Error occured while forcing uninstall of local cypress package")
+		return
+	}
+
+	output, err := exec.Command(
+		"npm",
+		"install",
+	).Output()
+	log.Debug().Msgf("NPM install output %s", string(output))
+
+	if err != nil {
+		log.Fatal().Err(err).Msgf("Error occured while installing user packages")
+		return
+	}
+
+	output, err = exec.Command(
+		"npm",
+		"install",
+		"--save-dev",
+		"mochawesome",
+	).Output()
+	log.Debug().Msgf("Mochawesome install output %s", string(output))
+
+	if err != nil {
+		log.Fatal().Err(err).Msgf("Error occured while installing mochawesome")
+		return
+	}
+
+	specs := strings.Split(c.Specs, ",")
+	if len(specs) > 0 {
+		var execErr []string
+		wg := sync.WaitGroup{}
+		wg.Add(len(specs))
+		for _, v := range specs {
+			go func(s string, c string, b string, execErr []string) {
+				f := filepath.Base(s)
+				args := []string{
+					"run",
+					"--browser",
+					b,
+					"--headless",
+					"--spec",
+					s,
+					"--reporter",
+					"mochawesome",
+					"--reporter-options",
+					fmt.Sprintf("reportFilename=%s", f),
+				}
+				log.Debug().Msgf("Running cypress command %s %s", "/usr/bin/cypress", strings.Join(args, " "))
+				output, err := exec.Command(
+					"/usr/bin/cypress",
+					"run",
+					"--browser",
+					b,
+					"--headless",
+					"--spec",
+					s,
+					"--reporter",
+					"mochawesome",
+					"--reporter-options",
+					fmt.Sprintf("reportFilename=%s", f),
+				).Output()
+				log.Debug().Msgf("Execution output %s", string(output))
+				if err != nil {
+					execErr = append(execErr, err.Error())
+				}
+				wg.Done()
+			}(v, c.ConfigFile, c.Browser, execErr)
+		}
+		wg.Wait()
+		if len(execErr) > 0 {
+			for i := range execErr {
+				log.Debug().Msgf("Execution error output %s", execErr[i])
+			}
+		} else {
+			log.Info().Msg("Program execution successful")
+		}
+	}
 }
--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/corpix/uarand v0.1.1 // indirect
 	github.com/go-git/go-git/v5 v5.3.0
 	github.com/icrowley/fake v0.0.0-20180203215853-4178557ae428
+	github.com/rs/zerolog v1.21.0
 	github.com/sergi/go-diff v1.2.0 // indirect
 	github.com/stretchr/testify v1.4.0
 	github.com/urfave/cli/v2 v2.3.0
