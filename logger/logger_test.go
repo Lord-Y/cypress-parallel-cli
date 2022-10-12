e65da33b7680fa790dae0f4133beac3013edc17e
Lord-Y <Lord-Y@users.noreply.github.com>
2022-10-12 11:09:46 +0200

    [logger] - Add env var CYPRESS_PARALLEL_CLI_LOG_LEVEL_WITH_CALLER

--- logger/logger_test.go
+++ logger/logger_test.go
@@ -15,42 +15,54 @@ func TestSetLoggerLogLevel(t *testing.T) {
 	tests := []struct {
 		logLevel string
 		expected string
+		caller   bool
 	}{
 		{
 			logLevel: "info",
 			expected: "info",
+			caller:   false,
 		},
 		{
 			logLevel: "warn",
 			expected: "warn",
+			caller:   true,
 		},
 		{
 			logLevel: "debug",
 			expected: "debug",
+			caller:   true,
 		},
 		{
 			logLevel: "error",
 			expected: "error",
+			caller:   true,
 		},
 		{
 			logLevel: "fatal",
 			expected: "fatal",
+			caller:   true,
 		},
 		{
 			logLevel: "trace",
 			expected: "trace",
+			caller:   true,
 		},
 		{
 			logLevel: "panic",
 			expected: "panic",
+			caller:   true,
 		},
 		{
 			logLevel: "plop",
 			expected: "info",
+			caller:   false,
 		},
 	}
 
 	for _, tc := range tests {
+		if tc.caller {
+			os.Setenv("CYPRESS_PARALLEL_CLI_LOG_LEVEL_WITH_CALLER", "true")
+		}
 		os.Setenv("CYPRESS_PARALLEL_CLI_LOG_LEVEL", tc.logLevel)
 		SetLoggerLogLevel()
 		z := zerolog.GlobalLevel().String()
